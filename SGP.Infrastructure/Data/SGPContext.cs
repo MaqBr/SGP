c61cd34ed478bf212d3f8ae8f6346187b0414dd2
MARCIO ALEXANDRE DE QUEIROZ <maqw3@hotmail.com>
2018-11-27 20:25:30 -0300

    Instala o EF Core e cria a classe contexto

---
+++ SGP.Infrastructure/Data/SGPContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGP.ApplicationCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGP.Infrastructure.Data
+{
+    public class SGPContext : DbContext
+    {
+        public SGPContext()
+        {
+
+        }
+
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<ItemDoPedido> ItensDosPedidos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BdSGP;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
